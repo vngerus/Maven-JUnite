74fbb33c196670579c940eeb0ee7b33499a221a7
angel <angelsmithlgs@gmail.com>
2024-08-27 14:52:14 -0400

    add: calculator con Junit 5

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,18 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    /**
+     * Se añaden integrales.
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public int add(int a, int b) {
+        return a + b;
     }
 }
